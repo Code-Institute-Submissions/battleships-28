2a0eb3e10564ae7603792374a76fa19981542979
Conor <conor.ganly@yahoo.com>
2022-04-06 20:02:50 +0000

    Add functionality to toggle instructions button

--- assets/js/battleships.js
+++ assets/js/battleships.js
@@ -1,21 +1,32 @@
 // Game object
 let game = {
     toggleGameOptions: () => {
-        let gameOptionsScreen = document.querySelector("#game-options-screen")
+        let gameOptionsScreen = document.querySelector("#game-options-screen");
         if(gameOptionsScreen.className){
-            gameOptionsScreen.classList.remove("hide")
+            gameOptionsScreen.classList.remove("hide");
         }
         else{
-            gameOptionsScreen.classList.add("hide")
+            gameOptionsScreen.classList.add("hide");
         }
+    },
+    toggleGameInstructions: () => {
+        let gameInstructions = document.querySelector("#instructions");
+        gameInstructions.showModal();
+        let closeButton = document.querySelector("#instructions-close-button");
+        closeButton.addEventListener("click", () => {
+            gameInstructions.close()
+        })
     }
 }
 
 // function to add event listeners without creating global variables
 const addListener = (id,typeOfEvent,eventLogic) => {
-    let target = document.getElementById(id)
-    target.addEventListener(typeOfEvent, eventLogic)
+    let element = document.getElementById(id)
+    element.addEventListener(typeOfEvent, eventLogic)
 }
 
 // Add functionality to game start up button in main menu.
 addListener("game-setup-button", "click", game.toggleGameOptions);
+
+// Add functionality to instructions button in main menu.
+addListener("instructions-button", "click", game.toggleGameInstructions);
