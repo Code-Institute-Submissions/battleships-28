09084c8a57cce6cc4fd34bffa32e36cc5a6c9263
Conor <conor.ganly@yahoo.com>
2022-04-06 16:49:32 +0000

    Adfunctionality to game setup button in main menu

--- assets/js/battleships.js
+++ assets/js/battleships.js
@@ -10,3 +10,12 @@ let game = {
         }
     }
 }
+
+// function to add event listeners without creating global variables
+const addListener = (id,typeOfEvent,eventLogic) => {
+    let target = document.getElementById(id)
+    target.addEventListener(typeOfEvent, eventLogic)
+}
+
+// Add functionality to game start up button in main menu.
+addListener("game-setup-button", "click", game.toggleGameOptions);
